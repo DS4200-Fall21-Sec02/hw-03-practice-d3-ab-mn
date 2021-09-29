4fb42d7704a4873eaaf06abb428a3f1b1f58733a
Alejandro Bermejo <alejandro@bermejo.app>
2021-09-29 18:58:43 -0400

    Scatterplot V2

--- js/main.js
+++ js/main.js
@@ -17,15 +17,7 @@ let svg1 = d3.select('#vis1')
   .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
   .style('background-color', '#ccc') // change the background color to light gray
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
-/*
-var bar = graph.selectAll("g") 
-   .data(data)
-   .enter()
-   .append("g")
-   .attr("transform", function(d, i) {
-      return "translate(0," + i * barHeight + ")";
-   });
-*/
+
 // second visualization
 let svg2 = d3.select('#vis2')
   .append('svg')
@@ -33,6 +25,7 @@ let svg2 = d3.select('#vis2')
   .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
   .style('background-color', '#ccc') // change the background color to light gray
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+
 
 let iris = d3.csv("data/iris.csv")
 iris.then(function(data) {
@@ -42,80 +35,90 @@ iris.then(function(data) {
     "Iris-versicolor": "green",
     "Iris-virginica": "blue"
   }
+
+  let speciesFormatted = {
+    "Iris-setosa": "Iris setosa",
+    "Iris-versicolor": "Iris versicolor",
+    "Iris-virginica": "Iris virginica"
+  }
   var xScale = d3.scaleLinear()
     .domain([
-      d3.min([0,d3.min(data,function (d) { return d.PetalWidthCm })]),
-      d3.max([0,d3.max(data,function (d) { return d.PetalWidthCm })])
+      d3.min(data,function (d) { return d.PetalWidthCm })-0.1,
+      d3.max(data,function (d) { return d.PetalWidthCm })
       ])
     .range([0,width])
+  console.log("AQUI")
+  console.log(d3.min(data,function (d) { return d.SepalLengthCm }))
   var yScale = d3.scaleLinear()
     .domain([
-      d3.min([0,d3.min(data,function (d) { return d.SepalLengthCm })]),
-      d3.max([0,d3.max(data,function (d) { return d.SepalLengthCm })])
+      d3.min(data,function (d) { return d.SepalLengthCm })-0.1,
+      d3.max(data,function (d) { return d.SepalLengthCm })
       ])
     .range([height,0])
   // SVG
-  var svg = svg2.append('svg')
-      .attr('height',height + margin.top + margin.bottom)
-      .attr('width',width + margin.left + margin.right)
-    .append('g')
+  var g = svg2.append('g')
       .attr('transform','translate(' + margin.left + ',' + margin.top + ')')
+  g.append('text')
+        .attr('x', width/2)
+        .attr('y', -20)
+        .attr('text-anchor', 'middle')
+        .style('font-size', 32)
+        .text('Size of Iris Species');
   // X-axis
   var xAxis = d3.axisBottom(xScale);
   // Y-axis
   var yAxis = d3.axisLeft(yScale);
   // Circles
-  var circles = svg.selectAll('circle')
+  var circles = g.selectAll('circle')
       .data(data)
       .enter()
     .append('circle')
       .attr('cx',function (d) { return xScale(d.PetalWidthCm) })
       .attr('cy',function (d) { return yScale(d.SepalLengthCm) })
-      .attr('r','10')
-      .attr('stroke','black')
-      .attr('stroke-width',1)
-      .attr('fill',function (d,i) { return colorScale[d.Species] })
+      .attr('r',7.5)
+      .attr('class','point')
+      .style('fill',function (d,i) { return colorScale[d.Species] })
       .on('mouseover', function () {
         d3.select(this)
           .transition()
           .duration(500)
-          .attr('r',20)
-          .attr('stroke-width',3)
+          .attr('class','selected')
+          .attr('stroke-width',2)
       })
       .on('mouseout', function () {
         d3.select(this)
           .transition()
           .duration(500)
-          .attr('r',10)
+          .attr('class', 'point')
           .attr('stroke-width',1)
       })
     .append('title') // Tooltip
-    .text(function (d) { return d.Species +
+    .text(function (d) { return 'Species: ' + speciesFormatted[d.Species] +
                            '\nPetal Width: ' + d.PetalWidthCm +
                            '\nSepal Length: ' + d.SepalLengthCm });
                            
   // X-axis
-  svg.append('g')
+  g.append('g')
       .attr('class','axis')
       .attr('transform', 'translate(0,' + height + ')')
       .call(xAxis)
-    .append('text') // X-axis Label
-      .attr('class','label')
-      .attr('y',-10)
+  g.append('text') // X-axis Label
+      .attr('class','axisLabel')
+      .attr('y',height-20)
       .attr('x',width)
-      .attr('dy','.71em')
+      .attr('dy','.6em')
       .style('text-anchor','end')
       .text('Petal Width')
   // Y-axis
-  svg.append('g')
+  g.append('g')
       .attr('class', 'axis')
       .call(yAxis)
-    .append('text') // y-axis Label
-      .attr('class','label')
+  g.append('text') // y-axis Label
+      .attr('class','axisLabel')
       .attr('transform','rotate(-90)')
       .attr('x',0)
       .attr('y',5)
-      .attr('dy','.71em')
+      .attr('dy','1em')
       .style('text-anchor','end')
       .text('Sepal Length')
 })
